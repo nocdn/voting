9a0639ab8d50808a37d7653bb52cdcfbda716b69
Bartek Bak <bartek.bak@protonmail.com>
2025-03-21 00:49:30 +0000

    feat: add a login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { createClient } from '@supabase/supabase-js';
+	const supabase = createClient(
+		'https://dduxgrvpsfbjilpaqyay.supabase.co',
+		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkdXhncnZwc2ZiamlscGFxeWF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1MDY5MzQsImV4cCI6MjA1ODA4MjkzNH0.xR5iQMZq5CyMv2xBkCQhpFf0HRNjkg_zqMooRiYyd28'
+	);
+
+	let passwordInput: string;
+	let emailInput: string;
+
+	async function supabaseLogin() {
+		console.log('login pressed');
+
+		const { data, error } = await supabase.auth.signInWithPassword({
+			email: emailInput,
+			password: passwordInput
+		});
+	}
+</script>
+
+<container class="w-vw grid h-dvh place-items-center">
+	<main class="flex flex-col gap-5 font-mono">
+		<p>voting page</p>
+		<form class="flex flex-col gap-1" autocomplete="off">
+			<input
+				type="email"
+				placeholder="email@example.com"
+				bind:value={emailInput}
+				class="focus:outline-none"
+			/>
+			<input
+				type="password"
+				placeholder="password"
+				bind:value={passwordInput}
+				class="focus:outline-none"
+			/>
+			<button
+				onclick={supabaseLogin}
+				class="mt-3 w-fit cursor-pointer border border-gray-200 px-3 py-2">login</button
+			>
+		</form>
+	</main>
+</container>
---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,3 @@
+<p>this is the admin page</p>
+
+<a href="/">log out</a>
